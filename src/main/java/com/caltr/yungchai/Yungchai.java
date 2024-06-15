636fe9154ee065926f1fcbfe0de13761b5349e35
Catsef <96400367+Catsef@users.noreply.github.com>
2024-06-15 20:28:15 +0800

    idk update

--- src/main/java/com/caltr/yungchai/Yungchai.java
+++ src/main/java/com/caltr/yungchai/Yungchai.java
@@ -1,17 +1,39 @@
 package com.caltr.yungchai;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Yungchai extends JavaPlugin {
+public final class Yungchai extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+
 
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable () {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin (PlayerJoinEvent event) {
+
+    }
+
+    @EventHandler
+    public void onDrop (PlayerDropItemEvent event) {
+
+    }
+
+    @EventHandler
+    public void onInteract (PlayerInteractEvent event) {
+
     }
 }
---
+++ src/main/java/com/caltr/yungchai/util.java
@@ -0,0 +1,9 @@
+package com.caltr.yungchai;
+
+public class util {
+
+    public static net.md_5.bungee.api.ChatColor asCol(String a) {
+        return net.md_5.bungee.api.ChatColor.of(a);
+    }
+
+}
